277fd378a8c4872b793b73be53e81d626488cabf
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-10-14 10:41:29 -0400

    write initial binary search function

--- script.ts
+++ script.ts
@@ -8,3 +8,17 @@ function linearSearch(array, term) {
 }
 
 console.log(linearSearch([47, 23, 66, "3", 2, 56, 3], 3));
+
+function binarySearch(array, term) {
+  const m = Math.floor((array.length - 1) / 2);
+  if (array.length === 0) {
+    return;
+  }
+  if (array[m] < term) {
+    return binarySearch(array.slice(m + 1, array.length), term);
+  } else if (array[m] > term) {
+    return binarySearch(array.slice(0, m + 1), term);
+  } else {
+    return m;
+  }
+}
