a73f51e8b3e18fcdff0271d9dd6cdc321f3b9296
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-10-14 11:56:55 -0400

    searches done

--- script.ts
+++ script.ts
@@ -28,3 +28,63 @@ function binarySearch(array, term, leading = 0) {
 }
 
 console.log(binarySearch([2, 3, 23, 47, 47, 56, 66], 47));
+
+// Define a tree as a list of lists
+const tree = [
+  {
+    value: 1,
+    children: [
+      { value: 2, children: [{ value: 5, children: [] }] },
+      { value: 3, children: [{ value: 6, children: [] }] },
+      { value: 3, children: [] },
+    ],
+  },
+  {
+    value: 1,
+    children: [
+      { value: 2, children: [{ value: 5, children: [] }] },
+      { value: 3, children: [{ value: 6, children: [] }] },
+      { value: 4, children: [] },
+    ],
+  },
+];
+
+function depthFirstSearch(tree, term, position: Array<any> = []) {
+  for (let i = 0; i < tree.length; i++) {
+    if (tree[i].value === term) {
+      position = position.concat(i);
+      return { position };
+    } else {
+      if (tree[i].children.length > 0) {
+        const d = depthFirstSearch(tree[i].children, term, position.concat(i));
+        if (d) {
+          return d;
+        }
+      }
+    }
+  }
+}
+
+// Example usage
+console.log(depthFirstSearch(tree, 4));
+
+function breadthFirstSearch(tree, term, position: Array<any> = []) {
+  const queue: Array<any> = [];
+  for (let i = 0; i < tree.length; i++) {
+    // console.log(tree, tree[i].children);
+    if (tree[i].value === term) {
+      return [i, term, position.concat(i)];
+    }
+    if (tree[i].children.length > 0) {
+      queue.push(tree[i].children);
+    }
+  }
+  for (let i = 0; i < queue.length; i++) {
+    const b = breadthFirstSearch(queue[i], term, position.concat([i]));
+    if (b) {
+      return b;
+    }
+  }
+}
+
+console.log(breadthFirstSearch(tree, 4));
